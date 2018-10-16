e006bb1c6f158ea6fcd069ed95f0115eb215d238
Mike Tschudi <mtschudi@esri.com>
2018-10-16 10:38:34 -0700

    Fixed copyright statements

--- dist/src/featureService.js
+++ dist/src/featureService.js
@@ -1,5 +1,18 @@
-/* Copyright (c) 2018 Esri
- * Apache-2.0 */
+/*
+ | Copyright 2018 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
 (function (factory) {
     if (typeof module === "object" && typeof module.exports === "object") {
         var v = factory(require, exports);
@@ -51,7 +64,7 @@
                     // the item and data sections with sections for the service, full layers, and
                     // full tables
                     // Add to the standard cost of creating an item: extra for creating because they're
-                    // slower than other items to create and extra because they have to be moved to the 
+                    // slower than other items to create and extra because they have to be moved to the
                     // desired folder
                     _this.estimatedCost += 2;
                     // Get the service description
